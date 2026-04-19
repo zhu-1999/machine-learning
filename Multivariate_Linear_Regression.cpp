31bd5010d4b76bd3ec724181e4b122ed82115e8e
zhu-1999 <zgy1091804195@mail.ustc.edu.cn>
2026-04-20 01:00:07 +0800

    first commit

---
+++ Multivariate_Linear_Regression.cpp
@@ -0,0 +1,111 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+using namespace std;
+vector<vector<double>> read_csv(string filename)
+{
+    vector<vector<double>> data;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        replace(line.begin(),line.end(),',',' ');
+        vector<double> row;
+        stringstream ss(line);
+        double num;
+        while (ss>>num)
+        {
+            row.push_back(num);
+        }
+        data.push_back(row);
+    }
+    return data;
+}
+void normalize(vector<vector<double>>& dataset){
+    double eps=1e-15;
+    for(int j=0;j<dataset[0].size()-1;j++){
+        double data_max=dataset[0][j],data_min=dataset[0][j];
+        for(int i=1;i<dataset.size();i++){
+            if(data_max<dataset[i][j]) data_max=dataset[i][j];
+            if(data_min>dataset[i][j]) data_min=dataset[i][j];
+        }
+        if(data_max==data_min){
+            for(int i=0;i<dataset.size();i++) dataset[i][j]=0;
+        }
+        else{
+            for(int i=0;i<dataset.size();i++) dataset[i][j]=(dataset[i][j]-data_min)/(data_max-data_min+eps);
+        }
+    }
+}
+vector<double> train_sgd(vector<vector<double>>& train,double learning_rate,int epochs){
+    int n=train[0].size();
+    vector<double>b(n,0);
+    for(int i=0;i<epochs;i++){
+        for(int j=0;j<train.size();j++){
+            double y_true=train[j][n-1],y_pred=b[0],error;
+            for(int k=0;k<n-1;k++) y_pred+=b[k+1]*train[j][k];
+            error=y_pred-y_true;
+            b[0]-=learning_rate*error;
+            for(int k=0;k<n-1;k++) b[k+1]-=learning_rate*error*train[j][k];
+        }
+    }
+    return b;
+}
+double RMSE(vector<vector<double>>& test, vector<double>& b){
+    double rmse=0;
+    for(int i=0;i<test.size();i++){
+        int n=b.size();
+        double y_pred=b[0];
+        for(int j=0;j<n-1;j++) y_pred+=b[j+1]*test[i][j];
+        rmse+=pow(y_pred-test[i][n-1],2);
+    }
+    return sqrt(rmse/test.size());
+}
+vector<vector<vector<double>>> k_fold_split(vector<vector<double>> dataset, int k){
+    shuffle(dataset.begin(), dataset.end(), mt19937(random_device{}()));
+    int n=dataset.size()/k;
+    vector<vector<vector<double>>> folds(k);
+    for(int i=0;i<k;i++){
+        int star=i*n,end=(i+1)*n-1;
+        if(i==k-1) end=dataset.size()-1;
+        for(int j=star;j<=end;j++) folds[i].push_back(dataset[j]);
+    }
+    return folds;
+}
+vector<double> evaluate(vector<vector<double>>& dataset, int k,double learning_rate,int epochs){
+    vector<vector<vector<double>>> folds=k_fold_split(dataset,k);
+    vector<double>scores;
+    for(int i=0;i<k;i++){
+        vector<vector<double>>test=folds[i],train;
+        for(int j=0;j<k;j++){
+            if(j!=i){
+                for(int s=0;s<folds[j].size();s++) train.push_back(folds[j][s]);
+            }
+        }
+        vector<double> b=train_sgd(train,learning_rate,epochs);
+        double score=RMSE(test,b);
+        scores.push_back(score);
+    }
+    return scores;
+}
+int main(){
+    srand(time(0)); 
+    vector<vector<double>> dataset = read_csv("insurance.csv");
+    vector<double> scores=evaluate(dataset,10,0.001,100);
+    for(int i=0;i<scores.size();i++){
+        cout << "╣з" << i+1 << "уш RMSE: " << scores[i] << endl;
+    }
+    double sum = 0;
+    for(int i=0;i<scores.size();i++) sum+=scores[i]; 
+    cout << "ф╫╬Ы RMSE: " << sum / scores.size() << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
---
+++ Simple_Linear_Regression.cpp
@@ -0,0 +1,116 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+using namespace std;
+vector<vector<double>> read_csv(string filename)
+{
+    vector<vector<double>> data;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        replace(line.begin(),line.end(),',',' ');
+        vector<double> row;
+        stringstream ss(line);
+        double num;
+        while (ss>>num)
+        {
+            row.push_back(num);
+        }
+        data.push_back(row);
+    }
+    return data;
+}
+double mean(vector<double>& value){
+    double sum=0;
+    for(int i=0;i<value.size();i++)
+        sum+=value[i];
+    return sum/value.size();
+}
+double variance(vector<double>& value,double mean_value){
+    double var=0;
+    for(int i=0;i<value.size();i++)
+        var+=(value[i]-mean_value)*(value[i]-mean_value);
+    return var;
+}
+double covariance(vector<double>& a,vector<double>& b,double mean_a,double mean_b){
+    double cov=0;
+    for(int i=0;i<a.size();i++)
+        cov+=(a[i]-mean_a)*(b[i]-mean_b);
+    return cov;
+}
+vector<double> coefficients(vector<vector<double>>& dataset){
+    vector<double>x,y;
+    for(int i=0;i<dataset.size();i++){
+        x.push_back(dataset[i][0]);
+        y.push_back(dataset[i][1]);
+    }
+    double mean_x,mean_y,cov,var_x,b0,b1;
+    mean_x=mean(x);
+    mean_y=mean(y);
+    cov=covariance(x,y,mean_x,mean_y);
+    var_x=variance(x,mean_x);
+    b1=cov/var_x;
+    b0=mean_y-b1*mean_x;
+    return {b0,b1};
+}
+
+double RMSE(vector<double>& y_true, vector<double>& y_pred){
+    double rmse=0;
+    for(int i=0;i<y_true.size();i++)
+        rmse+=(y_pred[i]-y_true[i])*(y_pred[i]-y_true[i]);
+    return sqrt(rmse/y_true.size());
+}
+vector<vector<vector<double>>> k_fold_split(vector<vector<double>> dataset, int k){
+    shuffle(dataset.begin(), dataset.end(), mt19937(random_device{}()));
+    int n=dataset.size()/k;
+    vector<vector<vector<double>>> folds(k);
+    for(int i=0;i<k;i++){
+        int star=i*n,end=(i+1)*n-1;
+        if(i==k-1) end=dataset.size()-1;
+        for(int j=star;j<=end;j++) folds[i].push_back(dataset[j]);
+    }
+    return folds;
+}
+vector<double> evaluate(vector<vector<double>>& dataset, int k){
+    vector<vector<vector<double>>> folds=k_fold_split(dataset,k);
+    vector<double>scores;
+    for(int i=0;i<k;i++){
+        vector<vector<double>>test=folds[i],train;
+        for(int j=0;j<k;j++){
+            if(j!=i){
+                for(int s=0;s<folds[j].size();s++) train.push_back(folds[j][s]);
+            }
+        }
+        vector<double> coeff=coefficients(train),y_true,y_pred;
+        double b0=coeff[0],b1=coeff[1];
+        for(int j=0;j<test.size();j++) {
+            y_true.push_back(test[j][1]);
+            y_pred.push_back(b1*test[j][0]+b0);
+        }
+        double score=RMSE(y_true,y_pred);
+        scores.push_back(score);
+    }
+    return scores;
+}
+int main(){
+    srand(time(0)); 
+    vector<vector<double>> dataset = read_csv("insurance.csv");
+    vector<double> scores=evaluate(dataset,10);
+    for(int i=0;i<scores.size();i++){
+        cout << "╣з" << i+1 << "уш RMSE: " << scores[i] << endl;
+    }
+    double sum = 0;
+    for(int i=0;i<scores.size();i++) sum+=scores[i]; 
+    cout << "ф╫╬Ы RMSE: " << sum / scores.size() << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
