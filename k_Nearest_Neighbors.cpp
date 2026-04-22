f3d64b371f08debdda1476c94e511024b45e86fd
zhu-1999 <zgy1091804195@mail.ustc.edu.cn>
2026-04-22 18:19:35 +0800

    update code

---
+++ k_Nearest_Neighbors.cpp
@@ -0,0 +1,111 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+using namespace std;
+vector<vector<double>> read_csv(string filename)
+{
+    vector<vector<double>> data;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        replace(line.begin(),line.end(),',',' ');
+        vector<double> row;
+        stringstream ss(line);
+        double num;
+        while (ss>>num)
+        {
+            row.push_back(num);
+        }
+        data.push_back(row);
+    }
+    return data;
+}
+void normalize(vector<vector<double>>& dataset){
+    double eps=1e-15;
+    for(int j=0;j<dataset[0].size()-1;j++){
+        double data_max=dataset[0][j],data_min=dataset[0][j];
+        for(int i=1;i<dataset.size();i++){
+            if(data_max<dataset[i][j]) data_max=dataset[i][j];
+            if(data_min>dataset[i][j]) data_min=dataset[i][j];
+        }
+        if(data_max==data_min){
+            for(int i=0;i<dataset.size();i++) dataset[i][j]=0;
+        }
+        else{
+            for(int i=0;i<dataset.size();i++) dataset[i][j]=(dataset[i][j]-data_min)/(data_max-data_min+eps);
+        }
+    }
+}
+double predict(vector<vector<double>>& train,vector<double>& row,int num_neighbor){
+    int n=train.size();
+    vector<vector<double>> dist(n,vector<double>(2,0));
+    for(int i=0;i<n;i++){
+        dist[i][1]=train[i].back();
+        for(int j=0;j<row.size()-1;j++) 
+            dist[i][0]+=pow(row[j]-train[i][j],2);
+    }
+    sort(dist.begin(),dist.end());
+    double sum=0;
+    for(int i=0;i<num_neighbor;i++){
+        sum+=dist[i][1];
+    }
+    return sum/num_neighbor;
+}
+double test_predict(vector<vector<double>>& train,vector<vector<double>>& test,int num_neighbor){
+    double rmse=0;
+    for(int i=0;i<test.size();i++){
+        double y_pred=predict(train,test[i],num_neighbor);
+        rmse+=pow(test[i].back()-y_pred,2);
+    }
+    return sqrt(rmse/test.size());
+}
+vector<vector<vector<double>>> k_fold_split(vector<vector<double>> dataset,int k){
+    shuffle(dataset.begin(), dataset.end(), mt19937(random_device{}()));
+    int n=dataset.size()/k;
+    vector<vector<vector<double>>> folds(k);
+    for(int i=0;i<k;i++){
+        int start=i*n,end=(i+1)*n-1;
+        if(i==k-1) end=dataset.size()-1;
+        for(int j=start;j<=end;j++) folds[i].push_back(dataset[j]);
+    }
+    return folds;
+}
+vector<double> evaluate(vector<vector<double>>& dataset, int k,int num_neighbor){
+    vector<vector<vector<double>>> folds=k_fold_split(dataset,k);
+    vector<double>scores;
+    for(int i=0;i<k;i++){
+        vector<vector<double>>test=folds[i],train;
+        for(int j=0;j<k;j++){
+            if(j!=i){
+                for(int s=0;s<folds[j].size();s++) train.push_back(folds[j][s]);
+            }
+        }
+        double score=test_predict(train,test,num_neighbor);
+        scores.push_back(score);
+    }
+    return scores;
+}
+
+int main(){
+    srand(time(0)); 
+    vector<vector<double>> dataset = read_csv("abalone.csv");
+    normalize(dataset);
+    vector<double> scores=evaluate(dataset,5,5);
+    for(int i=0;i<scores.size();i++){
+    cout << "Fold " << i+1 << " score: " << scores[i] << endl;
+    }
+    double sum = 0;
+    for(int i=0;i<scores.size();i++) sum += scores[i];
+    cout << "Average score: " << sum / scores.size() << endl;
+    cin.get();
+    cin.get();
+    return 0;
+}
